3edf15d08ef6e3c2637ec3e7d242eb37fc848c62
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2017-05-01 16:55:09 -0600

    Support for Hello NAME where NAME is the first argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
 
 int main(int argc, char * argv[]){
-	printf("Hello World\n");
+	if( argc > 1 ){
+		printf("Hello %s\n", argv[1]);
+	} else {
+		printf("Hello World\n");
+	}
 	return 0;
 }
