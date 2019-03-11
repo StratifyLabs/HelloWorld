922c91b268505be88c50b56c715d6cfe32d4fd16
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-03-11 17:16:29 -0600

    remove old doc folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 
 #include <cstdio>
-#include <sapi/sys.hpp>
-
 
 int main(int argc, char * argv[]){
-	Cli cli(argc, argv);
-	cli.set_publisher("Stratify Labs, Inc");
-	cli.handle_version();
 	printf("Hello World\n");
 	return 0;
 }
