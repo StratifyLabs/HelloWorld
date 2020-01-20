f65e7c140acbb2172a0d6f26a9f4ae1a5d17e3aa
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-01-20 09:38:10 -0700

    cmake updates

--- src/sl_config.h
+++ src/sl_config.h
@@ -9,6 +9,7 @@
 #define SL_CONFIG_VERSION_STRING "1.28"
 #define SL_CONFIG_VERSION_BCD 0x11C
 #define SL_CONFIG_DOCUMENT_ID "Kvp7xXzdO94kyCWAAcmW"
+#define SL_CONFIG_TEAM_ID ""
 #define SL_CONFIG_NAME "HelloWorld"
 #define SL_CONFIG_TYPE "app"
 #define SL_CONFIG_PUBLISHER "Stratify Labs, Inc"
