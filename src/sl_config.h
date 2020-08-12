5d61736141cd0d379de7d2a9c7164aaee7a991ba
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-08-12 12:15:51 -0600

    SDK PATH

--- src/sl_config.h
+++ src/sl_config.h
@@ -6,14 +6,14 @@
 #ifndef SL_CONFIG_H_
 #define SL_CONFIG_H_
 
-#define SL_CONFIG_VERSION_STRING "1.28"
-#define SL_CONFIG_VERSION_BCD 0x11C
-#define SL_CONFIG_DOCUMENT_ID "Kvp7xXzdO94kyCWAAcmW"
-#define SL_CONFIG_TEAM_ID ""
+#define SL_CONFIG_VERSION_STRING "1.0"
+#define SL_CONFIG_VERSION_BCD 0x100
+#define SL_CONFIG_DOCUMENT_ID "Qjc8Q2dy0M0O8KScO3JX"
+#define SL_CONFIG_TEAM_ID "41zVSUkV7jVX9bviWjpR"
 #define SL_CONFIG_NAME "HelloWorld"
 #define SL_CONFIG_TYPE "app"
 #define SL_CONFIG_PUBLISHER "Stratify Labs, Inc"
-#define SL_CONFIG_PERMISSIONS "public"
+#define SL_CONFIG_PERMISSIONS "private"
 #define SL_CONFIG_HARDWARE_ID_STRING ""
 
 #endif
